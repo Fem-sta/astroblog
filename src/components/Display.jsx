30e113c95592a2f5da4b4e048b12c0eedcf55b0d
Femi <femiaguaze2017@gmail.com>
2025-12-16 22:01:32 -0500

    Add two new blog entries

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,14 +1,21 @@
 export default function Display({imgPreview, keyName}){
       if (!imgPreview || !imgPreview[keyName]) return null;
 
-      const {src, alt} = imgPreview[keyName];
+      const {src, alt, type} = imgPreview[keyName];
     return(
         <div className="p-4 bg-gray-100 rounded mx-auto">
-        <video src={src} autoPlay
-        loop
-        muted
-        playsinline 
-        class="video w-full h-auto object-cover">   </video>
+            {type==="video"?(
+                <video src={src} autoPlay
+                loop
+                muted
+                playsinline 
+                class="video w-full h-auto object-cover"> </video>
+            ):(
+                <img
+                src={src}
+                alt={alt||""}
+                className="w-full h-auto object-cover"/>
+            )}
         </div>
 
     )
